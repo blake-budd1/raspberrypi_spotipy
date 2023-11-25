31f7cdfad08b3bdbcbf73dddd0a4108dc8b133ec
Blake Budd <70623089+blake-budd1@users.noreply.github.com>
2023-11-25 13:53:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from time import sleep
 import requests
+import urllib.request
+from PIL import Image
+import cv2
+#import screeninfo
+#import pyautogui
+import re
+
 
 MJ_URL = "http://the-modern-jukebox-react-app.vercel.app/api/messages"
 
@@ -24,15 +31,48 @@ while True:
             try:
                 x = requests.get(MJ_URL) # Get the spotify uri from the front-end queue
                 print(x.text)
+                # need to figure out which uri we are working with depending on what the queue holds:
+                
                 x.raise_for_status()
             except requests.exceptions.HTTPError as e:
                 print (e.response.text)
             
             # transfer playback device to the raspberry pi
             sp.transfer_playback(device_id=DEVICE_ID, force_play=False)
+
+            # deal with changing the image that is being displayed (update the album cover)
+            
+            #getting image; This will be the album cover that the uri provides
+            url = "https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228" #"https://img2.goodfon.com/wallpaper/nbig/1/e9/cat-kot-koshka-trava-chernyy.jpg"
+
+            #get image name
+            image = re.findall("[^/]+(?=/$|$)", url)[0]
             
             # start playing the uri that we got in x 
             sp.start_playback(device_id=DEVICE_ID, context_uri=x)
+
+            urllib.request.urlretrieve(url, image) #"cat-kot-koshka-trava-chernyy.jpg")
+
+            #load image
+            im = cv2.imread(image) #r"cat-kot-koshka-trava-chernyy.jpg")
+            
+            # improving resolution of image
+            hires = cv2.pyrUp(im)
+            hires2 = cv2.pyrUp(hires)
+            hires3 = cv2.pyrUp(hires2)
+            
+            #setting full screen
+            cv2.namedWindow("Album", cv2.WINDOW_NORMAL)
+            cv2.setWindowProperty("Album", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+            
+            #show image
+            cv2.imshow("Album", hires3)
+            
+            #show title bar
+            #cv2.setWindowTitle("Album", "Call Me Maybe")
+            
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
             
             # error checking for http requests
             try:
@@ -48,3 +88,12 @@ while True:
 
     finally:
         print("Cleaning  up...")
+
+
+
+
+
+
+
+
+
